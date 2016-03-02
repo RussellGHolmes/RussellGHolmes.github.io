43de53d76c9b3f2f36e20de0f3806c1edc23ac2d
russellgholmes1 <russell.g.holmes@gmail.com>
2016-03-02 01:27:11 +0000

    matchy

--- projects/matchy/my_code.js
+++ projects/matchy/my_code.js
@@ -3,3 +3,15 @@ animal.species = "dog";
 animal["name"] = "Spot"; 
 animal.noises = [];
 console.log(animal);
+
+var noises = [];
+noises[0] = "woof";
+noises.push("growl");
+noises.unshift("yip");
+noises[noises.length] = "howl";
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
+
+
+
