ac6b1089e7e63a56b87c9b409fa83e4a7bc69827
russellgholmes1 <russell.g.holmes@gmail.com>
2016-03-03 00:48:27 +0000

    matchy

--- projects/matchy/my_code.js
+++ projects/matchy/my_code.js
@@ -5,7 +5,7 @@ animal.noises = [];
 console.log(animal);
 
 var noises = [];
-noises[0] = "woof";
+noises[0] = "bark";
 noises.push("growl");
 noises.unshift("yip");
 noises[noises.length] = "howl";
@@ -13,10 +13,47 @@ console.log(noises.length);
 console.log(noises[noises.length - 1]);
 console.log(noises);
 
-animal.noises += noises;
+animal.noises = noises;
 animal.noises.push("whine");
 console.log(animal);
 
 var animals = [];
 animals.push(animal);
 console.log(animals);
+var duck = {
+    species: "duck",
+    name: "Jerome",
+    noises: ["quack", "honk", "sneeze", "woosh"]
+};
+animals.push(duck);
+console.log(animals);
+
+var cow = {
+    species: "cow",
+    name: "Bessie",
+    noises: ["moo", "sizzle", "would you like fries with that?", "eat more chicken"]
+};
+animals.push(cow);
+console.log(animals);
+
+var cat = {
+    species: "cat",
+    name: "Felix",
+    noises: ["meow", "hiss", "whine"]
+};
+animals.push(cat);
+console.log(animals);
+
+//Math.random returns a random number which can used to acces an array index//
+var friends = [];
+function random(){
+    var randomAnimal = animals[Math.floor(Math.random()*animals.length)];
+    return randomAnimal.name;
+}
+friends.push(random());
+console.log(friends);
+animals[2].friends = friends;
+
+
+
+
