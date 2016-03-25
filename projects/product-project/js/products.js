82e3e9e5eee58199131af3aef2d4741f480765cb
russellgholmes1 <russell.g.holmes@gmail.com>
2016-03-25 02:07:21 +0000

    product-project

--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -1,22 +1,22 @@
 /*global _*/
 (function () {
-    var products;
+    var allProducts;
     
     $.ajax("data/product.json")
         .done(function(result, status, request){
-            products = result;
-            console.log(products);
+            allProducts = result;
+            console.log(allProducts);
             initialize();
-            displayProducts();
+            displayProducts(allProducts);
         })
         .fail(function(request, status, error){
             console.log(status + ": " + error);
         });
         
     function filterProducts(searchTerm){
-        var filtered = _.reduce(products, function (p){
+        var filtered = _.filter(allProducts, function (p){
             return searchObject(searchTerm, p);
-            });          
+            }); 
             
         displayProducts(filtered);
     }
@@ -31,37 +31,45 @@
         }, false);
     }
     
-    function displayProducts(){
-        //var $products = $('#products');
-        //$products.empty();
-        var $products = $('<ul>');
+    function displayProducts(products){
+        var $products = $('#products');
+        $products.empty();
         var $listItems = _.map(products, function(p){
-            var $li = $('<li>');
+            var $li = $('<li>')
+                .on("click", function(e){
+                $("#product-details").fadeIn(400)});
             var $name = $('<p>').text(p.desc);
             var $price = $('<p>').text("Price: $ " + p.price);
             var $productColor = $('<p>').text("Shown Color: " + p.color);
             var $availableColors = $('<p>').text("Available Colors: " + p.availableColors);
             if (p.specs.length)
                 var $specs = $('<p>').text("Specifications: " + p.specs);
-            var $stock = $('<p>').text("Amount left in stock: " + p.stock);
+            if (p.stock < 15)    
+                var $stock = $('<p>').text("Only " + p.stock + " left in stock!");
+            else 
+                $stock = $('<p>').text("Number left in stock: " + p.stock);
             var $image = $('<img>').attr('src' , "img/product/thumbs/" + p.image);
             $li.append([$name, $specs, $price, $stock, $image, $productColor, $availableColors]);
             return $li;
         });
         $products.append($listItems);
-        $('body').append($products);
     }
     
     
     function initialize() {
-        displayProducts(products);
+        displayProducts(allProducts);
         $('#search').on('keyup', function (e){
             var term = $(e.target).val();
             if (term.length >= 3)
                 filterProducts(term);
             else 
-                displayProducts(products);
+                displayProducts(allProducts);
         });
+        
+        $(".close", "#product-details").on("click", function(e){
+            $("#product-details").fadeOut(400);
+        });
+        
     }
     
 })();
